71741936761d30f455ec0486310e559f73afe5dc
h-hr <106217548+h-hr@users.noreply.github.com>
2023-09-28 12:13:54 +0300

    Create Get_Selected.js

---
+++ pages/Get_Selected.js
@@ -0,0 +1,9 @@
+function getSelectedText() {
+    var selectedText = '';
+    if (window.getSelection) {
+        selectedText = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != 'Control') {
+        selectedText = document.selection.createRange().text;
+    }
+    return selectedText;
+}
